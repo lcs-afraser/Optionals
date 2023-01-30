26ae1142206d443befd5d3d4df63c365978d6718
Alistair Fraser <afraser@lcs.on.ca>
2023-01-30 09:21:37 -0500

    page 12

--- Optionals/Exploring Optionals.playground/Pages/Page 13.xcplaygroundpage/Contents.swift
+++ Optionals/Exploring Optionals.playground/Pages/Page 13.xcplaygroundpage/Contents.swift
@@ -10,7 +10,15 @@
  ![kc4](kc4.png)
  
  */
+//The answer will be cannot compute
 // Type the code below this line...
+var x: Int?
+var y: Int?
 
+if let notNilX = x, let notNilY=y {
+    notNilX * notNilY
+} else {
+    print("Cannot compute, x, y, or both are nil")
+}
 
 //: [Next](@next)
